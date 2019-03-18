587b1d121e61aa331d6ded66dbf6cb31dff7d76d
Jeroen Van-Hunen <Jeroen@Jeroens-Air-2.lan>
2019-03-18 21:17:30 +0000

    astyle

---
+++ unit_tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Jeroen/Box Sync/ASPECT/src/aspect/unit_tests
+# Build directory: /Users/Jeroen/Box Sync/ASPECT/src/aspect/unit_tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/Users/Jeroen/Box Sync/ASPECT/src/aspect/aspect" "--test")
+set_tests_properties(unit_tests PROPERTIES  WORKING_DIRECTORY "/Users/Jeroen/Box Sync/ASPECT/src/aspect/unit_tests")
